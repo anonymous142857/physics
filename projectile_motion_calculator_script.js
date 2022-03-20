2939a93c000381c6d298e75b4e3d61e69abd9457
qndks11 <36651226+qndks@users.noreply.github.com>
2022-03-20 13:57:30 +0900

    more functions

--- projectile_motion_calculator_script.js
+++ projectile_motion_calculator_script.js
@@ -2,10 +2,13 @@ let dv0 = document.getElementById("v0");
 let dtheta = document.getElementById("theta");
 let dy0 = document.getElementById("y0");
 let dg = document.getElementById("g");
+let drealR = document.getElementById("realR")
 let dresult = document.getElementById("result");
 let drresult = document.getElementById("rresult");
 let drrresult = document.getElementById("rrresult");
 let dtresult = document.getElementById("tresult");
+let dmresult = document.getElementById('mresult');
+let derrresult = document.getElementById('errresult');
 
 window.addEventListener('keypress', function(e) {
   if (e.code === "Enter") calculate();
@@ -20,14 +23,16 @@ function calculate() {
   let stheta = dtheta.value;
   let sy0 = dy0.value;
   let sg = dg.value;
-  if (sv0 === '' || stheta === '' || sy0 === '' || sg === '') return;
+  let smr = drealR.value;
+  if (sv0 === '' || stheta === '' || sy0 === '' || sg === '' || smr === '') return;
 
   let v0 = Number(sv0);
   let theta = Number(stheta);
   let y0 = Number(sy0);
   let g = Number(sg);
-  if (v0 === NaN || theta === NaN || y0 === NaN || g === NaN) return;
-  if (v0 <= 0 || theta <= 0 || theta >= 90 || y0 < 0 || g <= 0) return;
+  let mr = Number(smr);
+  if (v0 === NaN || theta === NaN || y0 === NaN || g === NaN || mr === NaN) return;
+  if (v0 <= 0 || theta <= 0 || theta >= 90 || y0 < 0 || g <= 0 || mr < 0) return;
 
   let rad = theta * Math.PI / 180;
 
@@ -36,8 +41,10 @@ function calculate() {
   let dr = rr-r;
   let dt = dr / (v0 * Math.cos(rad));
 
-  dresult.innerHTML = '수평도달거리: ' + round(r) + 'm';
-  drresult.innerHTML = '실제도달거리: ' + round(rr) + 'm';
   dtresult.innerHTML = '추가 진행시간: ' + round(dt) + 's';
   drrresult.innerHTML = '추가 도달거리: ' + round(dr) + 'm';
+  dmresult.innerHTML = '수평도달거리(측정): ' + round(mr-dr) + 'm';
+  dresult.innerHTML = '수평도달거리(이론): ' + round(r) + 'm';
+  drresult.innerHTML = '실제도달거리(이론): ' + round(rr) + 'm';
+  derrresult.innerHTML = '오차(%): ' + round(Math.abs(r-mr+dr) * 100/r);
 }
